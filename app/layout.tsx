2d784439d298c835778a39e1e424742298b93971
Jeffrey-Paul Aggrey <jeffreyaggrey@cvpcorp.com>
2024-06-09 12:03:30 -0500

    conect clerk to convex and setup handlers to query users

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ConvexProvider from "./providers/ConvexProvider";
+import ConvexClertProvider from "./providers/ConvexClerkProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvexProvider>{children}</ConvexProvider>
+        <ConvexClertProvider>{children}</ConvexClertProvider>
       </body>
     </html>
   );
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://stirring-meerkat-60.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
