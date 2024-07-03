028584e9a4fadd6af81951df97ccd7455a13c550
Jeffrey-Paul Aggrey <jeffreyaggrey@cvpcorp.com>
2024-07-02 21:23:11 -0500

    add interfaces and add skaffold GeneratePodcast and GenerateThumbnail

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -26,23 +26,42 @@ import {
 import { Textarea } from '@/components/ui/textarea';
 
 import { cn } from '@/lib/utils';
+import GeneratePodcast from '@/components/GeneratePodcast';
+import GenerateThumbnail from '@/components/GenerateThumbnail';
+import { Loader } from 'lucide-react';
+import { Id } from '@/convex/_generated/dataModel';
 
 const voiceCategories = ['alloy', 'shimmer', 'nova', 'echo', 'fable', 'onyx'];
 
 const formSchema = z.object({
-  podcastTitle: z.string().min(2, {
-    message: 'Username must be at least 2 characters.',
-  }),
+  podcastTitle: z.string().min(2),
+  podcastDescription: z.string().min(2),
 });
 
 const CreatePodcast = () => {
+  const [imagePrompt, setimagePrompt] = useState('');
+  const [imageStorageId, setImageStorageId] = useState<Id<'_storage'> | null>(
+    null
+  );
+  const [imageUrl, setImageUrl] = useState('');
+
+  const [audioUrl, setAudioUrl] = useState('');
+  const [audioStorageId, setAudioStorageId] = useState<Id<'_storage'> | null>(
+    null
+  );
+  const [audioDuration, setAudioDuration] = useState(0);
+
   const [voiceType, setVoiceType] = useState<string | null>(null);
+  const [voicePrompt, setVoicePrompt] = useState('');
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       podcastTitle: '',
+      podcastDescription: '',
     },
   });
 
@@ -138,6 +157,35 @@ const CreatePodcast = () => {
                 </FormItem>
               )}
             />
+          </div>
+
+          <div className='flex flex-col pt-10'>
+            <GeneratePodcast
+              setAudioStorageId={setAudioStorageId}
+              setAudio={setAudioUrl}
+              voiceType={voiceType}
+              audio={audioUrl}
+              voicePrompt={voicePrompt}
+              setVoicePrompt={setVoicePrompt}
+              setAudioDuration={setAudioDuration}
+            />
+            <GenerateThumbnail />
+
+            <div className='mt-10 w-full'>
+              <Button
+                type='submit'
+                className='text-16 w-full bg-orange-1 py-4 font-extrabold text-white-1 transition-all duration-500 hover:bg-black-1'
+              >
+                {isSubmitting ? (
+                  <>
+                    Submitting
+                    <Loader size={20} className='animate-spin ml-2' />
+                  </>
+                ) : (
+                  'Submit & Publish Podcast'
+                )}
+              </Button>
+            </div>
           </div>
         </form>
       </Form>
