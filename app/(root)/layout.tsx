5effba09c965152ee46fb528a0cff45b515669ae
Jeffrey-Paul Aggrey <jeffreyaggrey@cvpcorp.com>
2024-07-02 20:02:48 -0500

    complete first section of CreatePodcast form

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
-import LeftSideBar from "@/components/LeftSideBar";
-import MobileNav from "@/components/MobileNav";
-import RightSideBar from "@/components/RightSideBar";
-import Image from "next/image";
+import LeftSideBar from '@/components/LeftSideBar';
+import MobileNav from '@/components/MobileNav';
+import RightSideBar from '@/components/RightSideBar';
+import Image from 'next/image';
 
 export default function RootLayout({
   children,
@@ -9,22 +9,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="relative flex flex-col">
-      <main className="relative flex bg-black-3">
+    <div className='relative flex flex-col'>
+      <main className='relative flex bg-black-3'>
         <LeftSideBar />
 
-        <section className="flex flex-col flex-1 px-4 sm:px-14 min-h-screen">
-          <div className="mx-auto flex w-full max-w-5xl max-sm:px-4">
-            <div className="flex h-16 items-center justify-between md:hidden">
+        <section className='flex flex-col flex-1 px-4 sm:px-14 min-h-screen'>
+          <div className='mx-auto flex w-full max-w-5xl max-sm:px-4'>
+            <div className='flex h-16 items-center justify-between md:hidden'>
               <Image
-                src="icons/logo.svg"
-                alt="menu icon"
+                src='icons/logo.svg'
+                alt='menu icon'
                 width={30}
                 height={30}
               />
               <MobileNav />
             </div>
-            <div className="flex flex-col md:pb-14">
+            <div className='flex flex-col md:pb-14 w-full'>
               Toaster
               {children}
             </div>
