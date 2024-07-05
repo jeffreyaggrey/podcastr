db059b57d8706cfd504137cdc99e9bd25aca628d
Jeffrey-Paul Aggrey <jeffreyaggrey@cvpcorp.com>
2024-07-04 20:39:05 -0500

    implement mutations and queries for podcasts, PodcastDetail and PodcastDetailPlayer components

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,5 +1,6 @@
 import { PodcastCardProps } from '@/types';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const PodcastCard = ({
@@ -8,8 +9,16 @@ const PodcastCard = ({
   description,
   imgUrl,
 }: PodcastCardProps) => {
+  const router = useRouter();
+
+  const handleViews = () => {
+    // increase views
+
+    router.push(`/podcasts/${podcastId}`, { scroll: true });
+  };
+
   return (
-    <div className='cursor-pointer'>
+    <div className='cursor-pointer' onClick={handleViews}>
       <figure className='flex flex-col gap-2'>
         <Image
           src={imgUrl}
